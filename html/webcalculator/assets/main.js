0b3e301c08ac2cce30f28800bd3cee83d0e68d9b
yudaslegend <hdinjos@gmail.com>
2020-03-31 21:58:26 +0700

    finish calculator with js

--- html/webcalculator/assets/main.js
+++ html/webcalculator/assets/main.js
@@ -63,7 +63,15 @@ function performCalculaion(){
         result = parseInt(calculator.firstNumber) - parseInt(calculator.displayNumber);
     }
 
+    const history = {
+        firstNumber : calculator.firstNumber,
+        secondNumber : calculator.displayNumber,
+        operator : calculator.operator,
+        result : result
+    }
+    putHistory(history);
     calculator.displayNumber = result;
+    renderHistory();
 }
 
 //if click button
